eeb5e4e24846f91b68fe910eab5ff4397ba0bf07
Bias313 <mcgyver@hotmail.de>
2014-06-15 20:47:14 +0200

    Abgabefertig

--- RSA_Cosole/FileHandler.cs
+++ RSA_Cosole/FileHandler.cs
@@ -9,7 +9,13 @@ namespace RSA_Cosole
 {
     public class FileHandler
     {
-      
+        #region Read
+        /// <summary>
+        /// Tries to read file bytewise and returns success. The read file is written to the out variable.
+        /// </summary>
+        /// <param name="strFileName"></param> file to read
+        /// <param name="nArFile"></param> OUT - read File
+        /// <returns></returns>
         public bool TryReadFileByteWise(string strFileName, out byte[] nArFile)
         {
             bool blnSuccess = false;
@@ -21,12 +27,21 @@ namespace RSA_Cosole
                 blnSuccess = true;
             }
             catch
-            {}
+            { }
 
             return blnSuccess;
         }
+        
+        #endregion
 
-        public bool WriteFileByteWise(string strFileName, byte[] nArFile)
+        #region Write
+        /// <summary>
+        /// Writes byte array to the file
+        /// </summary>
+        /// <param name="strFileName">Target filename</param>
+        /// <param name="nArFile">File to Write</param>
+        /// <returns>Success</returns>
+        public bool WriteFile(string strFileName, byte[] nArFile)
         {
             bool blnSuccess = false;
             try
@@ -38,7 +53,29 @@ namespace RSA_Cosole
             { }
             return blnSuccess;
         }
+    
+        /// <summary>
+        /// Writes and int array converted to a byte array to the file
+        /// </summary>
+        /// <param name="strNewFile">Target filename</param>
+        /// <param name="nIntAr">Array to write to file</param>
+        /// <returns></returns>
+        public bool WriteFile(string strNewFile, int[] nIntAr)
+        {
+            byte[] nByteAr = IntArToByteAr(nIntAr);
+            return WriteFile(strNewFile, nByteAr);
+        }     
 
+        #endregion
+
+        #region Helpfull
+
+        /// <summary>
+        /// Appends an postfix to an existing filename while keeping the path and extension
+        /// </summary>
+        /// <param name="strFile">original filename</param>
+        /// <param name="strPostfix">postfix</param>
+        /// <returns></returns>
         public string GetNewFileName(string strFile, string strPostfix)
         {
             string strPath = Path.GetDirectoryName(strFile);
@@ -48,6 +85,21 @@ namespace RSA_Cosole
             strNewFile = Path.Combine(strPath, strNewFile);
             return strNewFile;
         }
+       
+        /// <summary>
+        /// Writes byte array to the file
+        /// </summary>
+        /// <param name="intAr">Int-Array to convert</param>
+        /// <returns>Byte-Array</returns>
+        public byte[] IntArToByteAr(int[] intAr)
+        {
+            byte[] nByteAr = new byte[intAr.Length * 4];
+            for (int i = 0; i < intAr.Length; i++)
+                Array.Copy(BitConverter.GetBytes(intAr[i]), 0, nByteAr, 4 * i, 4);
+            return nByteAr;
+        }
+        #endregion
 
+       
     }
 }
--- RSA_Cosole/Program.cs
+++ RSA_Cosole/Program.cs
@@ -8,153 +8,246 @@ using System.Threading.Tasks;
 
 namespace RSA_Cosole
 {
-    class Program
+    /// <summary>
+    /// Text based user interface for crypting and encrypting files via RSA
+    /// Author: Tobias Arendt
+    /// </summary>
+    class TuiRSA
     {
+        #region statics
         private static FileHandler m_FileHandler = null;
-        private static RSAHandler m_RSAHandler = null;
-      
+        private static RSAHandler m_RSAHandler = null; 
+        #endregion
 
+        #region Main
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args">No Arguments used in this program</param>
         static void Main(string[] args)
         {
             m_FileHandler = new FileHandler();
-            m_RSAHandler = new RSAHandler();
+            m_RSAHandler = new RSAHandler(10000);
             Menue();
-            
         }
+        
+        #endregion  
 
-        static void Menue()
-        {
-            bool blnExit = false;
-            
-             while (!blnExit)
-             {
-                 Console.Clear();
-                 DrawMenue();
+        #region Crypting
 
-
-                 ConsoleKey cki = Console.ReadKey().Key;
-                 switch (cki)
-                 {
-                     case ConsoleKey.E:
-                     case ConsoleKey.D:
-                         string strFile = InputFileName();
-                         byte[] nArFile = null;
-                         if ((!String.IsNullOrEmpty(strFile))&&m_FileHandler.TryReadFileByteWise(strFile,out nArFile))
-                         {
-                            Console.WriteLine("Do you want to see the file bytewise? <Y / AnyKey>");
-                           
-                            ConsoleKey ck = Console.ReadKey().Key;
-                         
-                            if (ck == ConsoleKey.Y)
-                            { 
-                                ShowByteAr(nArFile);
-                            }else{/*Do not show*/}
-
-                            if (cki == ConsoleKey.E)
-                            {
-                                HandleEncryption(nArFile,strFile);
-                            }
-                            else
-                            {
-                                HandleDecryption(nArFile,strFile);
-                            }
-                         }
-                         
-                         else
-                         {
-                             Console.WriteLine("Canceled!");
-                             Thread.Sleep(3000);
-                         }
-                         break;
-                     
-                     
-                     case ConsoleKey.Escape:
-                         blnExit = true;
-                         break;                 }
-             }
-        }
-
-        private static void HandleDecryption(byte[] nArCypher,string strFile)
-        {
-            long nN = InputNumber();           
-            byte[] nArMessage = null;
-            if (nN > 0)
-            {
-                nArMessage = m_RSAHandler.Decrypt(nArMessage, nN);
-            }
-            else
-            {
-                Console.WriteLine("Canceled!");
-                Thread.Sleep(3000);
-            }
-           
-        }
-
+        /// <summary>
+        /// Handles input of neccessary data for encrypting the message. Triggers the encrypting if inupt is valid.
+        /// </summary>
+        /// <param name="nArMessage">Plaintext, that will be encrypted.</param> 
+        /// <param name="strFile">Original filename. On writing the encrypted file it will be uses as prefix(Test.txt-->Test_Encrypted.txt)</param>
         private static void HandleEncryption(byte[] nArMessage, string strFile)
         {
-            byte[] nArCypher = null;
-            long nPrime1 = InputPrimeNumber();
+            int[] nArCypher = null;
+            int nPrime1 = InputPrimeNumber();
             if (nPrime1 > 0)
             {
-                long nPrime2 = InputPrimeNumber();
+                int nPrime2 = InputPrimeNumber();
                 if (nPrime2 > 0)
                 {
-                    Int64 nN;
-                    nArCypher = m_RSAHandler.Encrypt(nArMessage,nPrime1,nPrime2,out nN);
-                    Console.WriteLine("Ecryption done. N: "+((long)nN).ToString());
+                    int nN;
+                    int nD;
+                    nArCypher = m_RSAHandler.Encrypt(nArMessage, nPrime1, nPrime2, out nN, out nD);
 
-                    Console.WriteLine("Do you want to see the encrypted file bytewise? <Y / AnyKey>");
+
+                    Console.WriteLine("Show encrypted file bytewise? <Y / AnyKey>");
                     ConsoleKey ck = Console.ReadKey().Key;
                     if (ck == ConsoleKey.Y)
                     {
-                        ShowByteAr(nArCypher);
+                        ShowInput(nArCypher);
                     }
                     else { /*Do not show*/}
-           
+
+                    Console.WriteLine(String.Format("Ecryption done. Note down: n={0}  d={1}", nN, nD));
+
                     string strNewFile = m_FileHandler.GetNewFileName(strFile, "_Encrypted");
                     try
                     {
-                        m_FileHandler.WriteFileByteWise(strNewFile, nArCypher);
-                        Console.WriteLine("Encrypted file written: "+strNewFile);
+                        m_FileHandler.WriteFile(strNewFile, nArCypher);
+                        Console.WriteLine("Encrypted file written: " + strNewFile);
                     }
                     catch
                     {
                         Console.WriteLine("Error while writing encrypted file: " + strNewFile);
                     }
+                    Console.ReadKey();
+
 
 
                 }
                 else
                 {
                     Console.WriteLine("Canceled!");
-                    Thread.Sleep(3000);
+
                 }
             }
             else
             {
                 Console.WriteLine("Canceled!");
-                Thread.Sleep(3000);
+            }
+            Thread.Sleep(3000);//Let the user time to read it.
+        }
+
+        /// <summary>
+        /// Handles input of neccessary data for decrypting the message. Triggers the decrypting if inupt is valid.
+        /// </summary>
+        /// <param name="nArCypher"> Plaintext, that will be encrypted.</param>
+        /// <param name="strFile">Original filename. On writing the decrypted file it will be uses as prefix(Test.txt-->Test_Decrypted.txt)</param>
+        private static void HandleDecryption(byte[] nArCypher, string strFile)
+        {
+            Console.WriteLine("\r\nPublic key n");
+            int nN = InputNumber();
+            byte[] nArMessage = null;
+            if (nN > 0)
+            {
+                Console.WriteLine("\r\nPublic key d");
+                int nD = InputNumber();
+                if (nD > 0)
+                {
+                    nArMessage = m_RSAHandler.Decrypt(nArCypher, nN, nD);
+                    Console.WriteLine("Show the decrypted file bytewise? <Y / AnyKey>");
+                    ConsoleKey ck = Console.ReadKey().Key;
+                    if (ck == ConsoleKey.Y)
+                    {
+                        ShowInput(nArMessage);
+                    }
+                    else { /*Do not show*/}
+                    string strNewFile = m_FileHandler.GetNewFileName(strFile, "_Decrypted");
+                    try
+                    {
+                        m_FileHandler.WriteFile(strNewFile, nArMessage);
+                        Console.WriteLine("Decrypted file written: " + strNewFile);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error while writing decrypted file: " + strNewFile);
+                    }
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Canceled!");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Canceled!");
+
+            }
+
+        }
+        #endregion
+
+        #region Output
+
+        /// <summary>
+        /// Show the an int array intwise.
+        /// </summary>
+        /// <param name="nAr">The array, we want to show in the onsole</param>
+        private static void ShowInput(int[] nAr)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < nAr.Length; i++)
+            {
+                Console.WriteLine(nAr[i].ToString().PadLeft(3, ' '));
             }
         }
 
-      
-
-        private static void ShowByteAr(byte[] nArFile)
-        {
-            throw new NotImplementedException();
+        /// <summary>
+        /// Show the an byte array bytewise in binary, decimal, hexadecimal and char view.
+        /// </summary>
+        /// <param name="nAr"></param>The array, we want to shown in console
+        private static void ShowInput(byte[] nAr)
+        {           
+            string[] strOutputAsHex = (BitConverter.ToString(nAr)).Split('-');//Get Hexstring and split it into an Array
+            Console.WriteLine("");
+            for (int i = 0; i < nAr.Length; i++)
+            {
+                string strLine = String.Format("{0}   {1}   {2}   {3}", Convert.ToString(nAr[i], 2).PadLeft(8, '0'), nAr[i].ToString().PadLeft(3), strOutputAsHex[i], System.Text.Encoding.UTF8.GetString(new byte[1] { nAr[i] }));
+                Console.WriteLine(strLine);
+            }
         }
 
+        /// <summary>
+        /// Show all options
+        /// </summary>
         private static void DrawMenue()
         {
             Console.WriteLine("RSA:\r\n");
-            Console.WriteLine("EnCrypt file    <E>");
-            Console.WriteLine("Decrypt file   <D>");
-            Console.WriteLine("Exit         <ESC>\r\n");
+            Console.WriteLine("Encrypt file    <E>");
+            Console.WriteLine("Decrypt file    <D>");
+            Console.WriteLine("Exit           <ESC>\r\n");
+        } 
+        #endregion
 
+        #region Input
+
+        /// <summary>
+        /// Interaction with user. Shows all options and waits for input.
+        /// </summary>
+        static void Menue()
+        {
+            bool blnExit = false;
+
+            while (!blnExit)
+            {
+                Console.Clear();
+                DrawMenue();
+
+
+                ConsoleKey cki = Console.ReadKey().Key;
+                switch (cki)//Whichs key was pressed?
+                {
+                    case ConsoleKey.E:
+                    case ConsoleKey.D:
+                        string strFile = InputFileName();
+                        byte[] nArFile = null;
+                        if ((!String.IsNullOrEmpty(strFile)) && m_FileHandler.TryReadFileByteWise(strFile, out nArFile))
+                        {
+                            Console.WriteLine("Show the file bytewise? <Y / AnyKey>");
+
+                            ConsoleKey ck = Console.ReadKey().Key;
+
+                            if (ck == ConsoleKey.Y)
+                            {
+                                ShowInput(nArFile);
+                            }
+                            else { /*Do not show*/}
+
+                            if (cki == ConsoleKey.E)
+                            {
+                                HandleEncryption(nArFile, strFile);
+                            }
+                            else
+                            {
+                                HandleDecryption(nArFile, strFile);
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Canceled!");
+                            Thread.Sleep(3000);
+                        }
+                        break;
+
+
+                    case ConsoleKey.Escape:
+                        blnExit = true;
+                        break;
+                }
+            }
         }
 
-
-
+        /// <summary>
+        /// Handles the Input of a prime number.
+        /// </summary>
+        /// <returns>Primenumber. If user breaks up, 0 will be returned./returns>
         private static int InputPrimeNumber()
         {
             int nRet = 0;
@@ -164,7 +257,7 @@ namespace RSA_Cosole
             bool blnExit = false;
             string strPath = new FileInfo(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName).Directory.FullName;
             Console.WriteLine("\r\n\r\n");
-            while (!blnIsValid && !blnExit)
+            while (!blnIsValid && !blnExit)//Neither a valid primenumber nor the user wants to cancel.
             {
 
                 Console.WriteLine("Enter a valid primenumber between 100 and 10,000:");
@@ -181,13 +274,13 @@ namespace RSA_Cosole
                         blnIsValid = true;
                     }
                     else
-                    {
+                    {//Not a prime number - inform user and ask again
                         Console.Clear();
                         Console.WriteLine("Not a primenumner between 100 and 10,000: " + strCur);
                     }
                 }
                 else
-                {
+                {//Not a number - inform user and ask again
                     Console.Clear();
                     Console.WriteLine("Not a primenumner between 100 and 10,000: " + strCur);
                     Console.WriteLine("Enter \"cancel\" to get back to the main-menue");
@@ -197,6 +290,10 @@ namespace RSA_Cosole
             return nRet;
         }
 
+        /// <summary>
+        /// Handles the Input of a number.
+        /// </summary>
+        /// <returns>The user input</returns>
         private static int InputNumber()
         {
             int nRet = 0;
@@ -205,8 +302,8 @@ namespace RSA_Cosole
             bool blnIsValid = false;
             bool blnExit = false;
             string strPath = new FileInfo(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName).Directory.FullName;
-            Console.WriteLine("\r\n\r\n");
-            while (!blnIsValid && !blnExit)
+            Console.WriteLine("\r\n");
+            while (!blnIsValid && !blnExit)//Neither an valid number nor the user wants to cancel.
             {
 
                 Console.WriteLine("Enter a valid number:");
@@ -217,11 +314,11 @@ namespace RSA_Cosole
                 }
                 else if (Int32.TryParse(strCur, out nCur))
                 {
-                        nRet = nCur;
-                        blnIsValid = true;                   
+                    nRet = nCur;
+                    blnIsValid = true;
                 }
                 else
-                {
+                {//Not a number - inform user and ask again
                     Console.Clear();
                     Console.WriteLine("Not valid number: " + strCur);
                     Console.WriteLine("Enter \"cancel\" to get back to the main-menue");
@@ -231,8 +328,11 @@ namespace RSA_Cosole
             return nRet;
         }
 
-        
 
+        /// <summary>
+        /// Handles input of an valid(existing) filename
+        /// </summary>
+        /// <returns>The user input</returns>
         private static string InputFileName()
         {
             string strRet = String.Empty;
@@ -241,35 +341,36 @@ namespace RSA_Cosole
             bool blnExit = false;
             string strPath = new FileInfo(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName).Directory.FullName;
             Console.Clear();
-            while (!blnIsValid&&!blnExit)
+            while (!blnIsValid && !blnExit)
             {
-                
+
                 Console.WriteLine("Enter a valid filename:");
                 strCur = Console.ReadLine();
                 if (strCur.ToLower() == "cancel")
                 {
                     blnExit = true;
                 }
-                else if (File.Exists(Path.Combine(strPath, strCur)))
-                {
+                else if (File.Exists(Path.Combine(strPath, strCur)))//check if file is in executable dir
+                {//Cool
                     strRet = Path.Combine(strPath, strCur);
                     blnIsValid = true;
                 }
-                else if (File.Exists(strCur))
-                {
+                else if (File.Exists(strCur))//check path easily
+                {//Cool
                     strRet = strCur;
                     blnIsValid = true;
                 }
-                else 
-                { 
+                else
+                { //Not a - inform user and ask again
                     Console.Clear();
-                    Console.WriteLine("Invalid File: "+strCur);
+                    Console.WriteLine("Invalid File: " + strCur);
                     Console.WriteLine("Enter \"cancel\" to get back to the main-menue");
                 }
             }
 
             return strRet;
-        }
+        } 
+        #endregion
 
     }
 }
